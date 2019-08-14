d0404f864d1cd9aaad8e858928848316b5532109
udayakiranvk <sannyj@gmail.com>
2019-08-13 23:36:39 -0500

    UDemy course tryouts

---
+++ Hello/AnotherHello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hey there,")
+	fmt.Println("Hello, World")
+
+	var code = "Ace of Spades"
+	code1 := 123
+	fmt.Println(code)
+
+	fmt.Println(code1)
+
+}
---
+++ Hello/Test1.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	deckSize = 50
+	fmt.Println(deckSize)
+}
